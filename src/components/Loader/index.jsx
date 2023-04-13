643c75165c78da5b93c6928d00549c131d6a62c5
Muhammad Bilal Aleem <30667628+brad911@users.noreply.github.com>
2023-04-14 02:44:10 +0500

    added FF logo on loader

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import gif from "../../assets/loading.gif";
 
 const Loader = () => {
   return (
-    <div className="preloader">
-      <div className="lds-hourglass"></div>
+    <div className="preloader" style={{ backgroundColor: "#2B6486" }}>
+      {/* <div className="lds-hourglass"></div> */}
+      <img src={gif}></img>
     </div>
   );
 };
